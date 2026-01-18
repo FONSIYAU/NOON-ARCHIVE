7f27d5de6ad6cbbf5420d26abfe7f98ab402c92a
FONSIYAU <78067765+FONSIYAU@users.noreply.github.com>
2026-01-19 00:47:13 +0800

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,15 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { StudioPage } from './components/Studio'
+import './index.css'
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+// 简单的路由判断
+const pathname = window.location.pathname
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
+if (pathname.startsWith('/studio')) {
+  // 如果网址里带 /studio，就渲染后台管理界面
+  root.render(
+    <React.StrictMode>
+      <StudioPage />
+    </React.StrictMode>
+  )
+} else {
+  // 否则渲染正常的商店前台
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
 }
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
